9ea1f6c92b811715ee9ef0d2cf06729c950eb8e9
ulises-jeremias <ulisescf.24@gmail.com>
2021-04-16 20:49:19 -0300

    Updated tests

--- rxv/onecontext/onecontext_test.v
+++ rxv/onecontext/onecontext_test.v
@@ -34,7 +34,7 @@ fn test_merge_nomilan() {
 	ctx := onecontext.merge(ctx1, ctx2)
 
 	if deadline := ctx.deadline() {
-		assert deadline.unix_time() == 0
+		panic('this should never happen')
 	}
 
 	val1_ptr := ctx.value('foo') or { panic('wrong value access for key `foo`') }
@@ -53,4 +53,93 @@ fn test_merge_nomilan() {
 	onecontext.cancel(ctx)
 	assert eventually(ctx.done())
 	assert ctx.err() is Error
+}
+
+fn test_merge_deadline_context_1() {
+	ctx1 := context.with_timeout(context.background(), time.second)
+	defer {
+		context.cancel(ctx1)
+	}
+	ctx2 := context.background()
+	ctx := onecontext.merge(ctx1, ctx2)
+
+	if deadline := ctx.deadline() {
+		assert deadline.unix_time() != 0
+	} else {
+		panic('this should never happen')
+	}
+}
+
+fn test_merge_deadline_context_2() {
+	ctx1 := context.background()
+	ctx2 := context.with_timeout(context.background(), time.second)
+	defer {
+		context.cancel(ctx2)
+	}
+	ctx := onecontext.merge(ctx1, ctx2)
+
+	if deadline := ctx.deadline() {
+		assert deadline.unix_time() != 0
+	} else {
+		panic('this should never happen')
+	}
+}
+
+fn test_merge_deadline_context_n() {
+	ctx1 := context.background()
+
+	mut ctxs := []context.Context{cap: 21}
+	for i in 0 .. 10 {
+		ctxs << context.background()
+	}
+	ctx_n := context.with_timeout(context.background(), time.second)
+	ctxs << ctx_n
+
+	for i in 0 .. 10 {
+		ctxs << context.background()
+	}
+
+	ctx := onecontext.merge(ctx1, ...ctxs)
+
+	assert !eventually(ctx.done())
+	assert ctx.err() is none
+	onecontext.cancel(ctx)
+	assert eventually(ctx.done())
+	assert ctx.err() is Error
+}
+
+fn test_merge_deadline_none() {
+	ctx1 := context.background()
+	ctx2 := context.background()
+
+	ctx := onecontext.merge(ctx1, ctx2)
+
+	if _ := ctx.deadline() {
+		panic('this should never happen')
+	}
+}
+
+fn test_merge_cancel_two() {
+	ctx1 := context.background()
+	ctx2 := context.background()
+
+	ctx := onecontext.merge(ctx1, ctx2)
+	onecontext.cancel(ctx)
+
+	assert eventually(ctx.done())
+	assert ctx.err() is Error
+	assert ctx.err().str() == 'canceled context'
+}
+
+fn test_merge_cancel_multiple() {
+	ctx1 := context.background()
+	ctx2 := context.background()
+	ctx3 := context.background()
+
+	ctx := onecontext.merge(ctx1, ctx2, ctx3)
+	onecontext.cancel(ctx)
+
+	assert eventually(ctx.done())
+	assert ctx.err() is Error
+	assert ctx.err().str() == 'canceled context'
 }
