1e0ed3c6aadc5025313d7d3e62beefb9e7a229e5
ulises-jeremias <ulisescf.24@gmail.com>
2021-06-26 00:01:14 -0300

    Updated iterables

--- rxv/iterable_channel.v
+++ rxv/iterable_channel.v
@@ -1,4 +1,72 @@
 module rxv
 
+import context
+
 struct ChannelIterable {
+	next                     chan Item
+	opts                     []RxOption
+	subscribers              shared []chan Item
+	producer_already_created shared bool
+}
+
+fn new_channel_iterable(next chan Item, opts ...RxOption) Iterable {
+	return &ChannelIterable{
+		next: next
+		opts: opts
+	}
+}
+
+pub fn (mut i ChannelIterable) observe(opts ...RxOption) chan Item {
+	mut options := i.opts.clone()
+	options << opts.clone()
+	option := parse_options(...options)
+
+	if !option.is_connectable() {
+		return i.next
+	}
+
+	if option.is_connect_operation() {
+		i.connect(option.build_context(voidptr(0)))
+	}
+
+	ch := option.build_channel()
+
+	lock i.subscribers {
+		i.subscribers << ch
+	}
+
+	return ch
+}
+
+fn (mut i ChannelIterable) connect(ctx context.Context) {
+	lock i.producer_already_created {
+		go i.produce(ctx)
+		i.producer_already_created = true
+	}
+}
+
+fn (mut i ChannelIterable) produce(ctx context.Context) {
+	defer {
+		rlock i.subscribers {
+			for subscriber in i.subscribers {
+				subscriber.close()
+			}
+		}
+	}
+
+	cdone := ctx.done()
+
+	for select {
+		_ := <-cdone {
+			return
+		}
+		item := <-i.next {
+			rlock i.subscribers {
+				for subscriber in i.subscribers {
+					subscriber <- item
+				}
+			}
+		}
+	} {
+	}
 }
---
+++ rxv/iterable_defer.v
@@ -0,0 +1,33 @@
+module rxv
+
+import context
+
+struct DeferIterable {
+	fs   []Producer
+	opts []RxOption
+}
+
+fn new_defer_iterable(fs []Producer, opts ...RxOption) Iterable {
+	return &DeferIterable{
+		fs: fs
+		opts: opts
+	}
+}
+
+pub fn (i &DeferIterable) observe(opts ...RxOption) chan Item {
+	mut options := i.opts.clone()
+	options << opts.clone()
+	option := parse_options(...options)
+	ctx := option.build_context(voidptr(0))
+	next := option.build_channel()
+
+	go fn (i &DeferIterable, next chan Item, ctx context.Context) {
+		defer {
+			next.close()
+		}
+		for f in i.fs {
+			f(ctx, next)
+		}
+	}(i, next, ctx)
+	return next
+}
