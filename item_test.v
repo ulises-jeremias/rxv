97f3ba1b9e7ea5e8440b3dbd985d459794705cd0
ulises-jeremias <ulisescf.24@gmail.com>
2021-12-28 21:49:24 -0300

    Added assertions

--- item_test.v
+++ item_test.v
@@ -33,7 +33,9 @@ fn test_send_items_variadic_with_error() {
 		val: 3
 	})]
 	mut bctx := context.background()
+	mut iter := new_channel_iterable(ch)
 	go send_items(mut &bctx, ch, .close_channel, items)
+	test(mut &bctx, mut &iter, has_items(items[0], items[1]), has_error(err))
 }
 
 fn test_send_items_slice() {
@@ -47,8 +49,10 @@ fn test_send_items_slice() {
 		ItemValue(&Value{
 		val: 3
 	})]
+	mut iter := new_channel_iterable(ch)
 	mut bctx := context.background()
 	go send_items(mut &bctx, ch, .close_channel, items_slice)
+	test(mut &bctx, mut &iter, has_items(...items_slice), has_no_error())
 }
 
 fn test_send_items_slice_with_error() {
@@ -62,8 +66,10 @@ fn test_send_items_slice_with_error() {
 	}), ItemValue(err), ItemValue(&Value{
 		val: 3
 	})]
+	mut iter := new_channel_iterable(ch)
 	mut bctx := context.background()
 	go send_items(mut &bctx, ch, .close_channel, items_slice)
+	test(mut &bctx, mut &iter, has_items(items_slice[0], items_slice[1]), has_error(err))
 }
 
 fn test_item_send_blocking() {
