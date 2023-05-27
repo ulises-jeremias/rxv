a15ee4802dd9c5b4672b09811b0350b4e3c0ff51
ulises-jeremias <ulisescf.24@gmail.com>
2023-05-27 17:46:24 -0300

    Working for now

--- examples/main.v
+++ examples/main.v
@@ -11,12 +11,12 @@ fn main() {
 
 	ch := chan rxv.Item{cap: 3}
 
-	ch <- rxv.of(rxv.new_item_value(0.0))
-	ch <- rxv.of(rxv.new_item_value(1.0))
-	ch <- rxv.of(rxv.new_item_value(2.0))
+	ch <- rxv.of(rxv.ItemValue(0.0))
+	ch <- rxv.of(rxv.ItemValue(1.0))
+	ch <- rxv.of(rxv.ItemValue(2.0))
 
-	mut obs := rxv.from_channel(ch)
-	mut avgf64 := obs.average_f64(...[]rxv.RxOption{})
+	// mut obs := rxv.from_channel(ch)
+	// mut avgf64 := obs.average_f64(...[]rxv.RxOption{})
 
-	rxv.assert_single(mut ctx, mut avgf64, rxv.has_items(rxv.new_item_value(1.0)))
+	// rxv.assert_single(mut ctx, mut avgf64, rxv.has_items(rxv.ItemValue(1.0)))
 }
