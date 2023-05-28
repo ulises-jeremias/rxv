4a18ff170e8a4e6184e7f675aa2b083baf581213
ulises-jeremias <ulisescf.24@gmail.com>
2023-05-28 17:40:13 -0300

    Added type

---
+++ src/optionalsingle.v
@@ -0,0 +1,11 @@
+module rxv
+
+import context
+
+// OptionalSingle is an optional single
+pub interface OptionalSingle {
+	Iterable // mut:
+	// 	get(opts ...RxOption) ?Item
+	// 	map(apply Func, opts ...RxOption) Single
+	// 	run(opts ...RxOption) chan int
+}
