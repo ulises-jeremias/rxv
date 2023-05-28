d18cd588d598726968767324f748bc4428c6153e
ulises-jeremias <ulisescf.24@gmail.com>
2023-05-28 02:30:30 -0300

    Working for now

--- examples/main.v
+++ examples/main.v
@@ -16,7 +16,7 @@ fn main() {
 	ch <- rxv.of(rxv.ItemValue(2.0))
 
 	// mut obs := rxv.from_channel(ch)
-	// mut avgf64 := obs.average_f64(...[]rxv.RxOption{})
+	// mut avgf64 := obs.average_f64()
 
 	// rxv.assert_single(mut ctx, mut avgf64, rxv.has_items(rxv.ItemValue(1.0)))
 }
