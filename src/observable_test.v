9316d207fba4b1980a2d130c386a9d91c8f2036a
ulises-jeremias <ulisescf.24@gmail.com>
2023-06-04 21:24:16 -0300

    Updated tests

--- src/observable_test.v
+++ src/observable_test.v
@@ -2,7 +2,7 @@ module rxv
 
 import context
 
-fn test_all_int() {
+fn test_all_int_true() {
 	mut bctx := context.background()
 	mut ctx, cancel := context.with_cancel(mut bctx)
 
@@ -12,21 +12,38 @@ fn test_all_int() {
 
 	ch := chan Item{cap: 8}
 
-	ch <- of(0.0)
-	ch <- of(1.0)
 	ch <- of(0)
-	ch <- of(2.0)
 	ch <- of(1)
-	ch <- of(3)
-	ch <- of(-1.0)
-	ch <- of(-2.0)
+	ch <- of(2)
 
 	mut obs := from_channel(ch)
 	mut all := obs.all(fn (value ItemValue) bool {
 		return value is int
 	})
 
-	assert_single(mut ctx, mut all, has_items(false, false, false, false, false))
+	assert_single(mut ctx, mut all, has_item(true))
+}
+
+fn test_all_int_false() {
+	mut bctx := context.background()
+	mut ctx, cancel := context.with_cancel(mut bctx)
+
+	defer {
+		cancel()
+	}
+
+	ch := chan Item{cap: 8}
+
+	ch <- of(0)
+	ch <- of('1')
+	ch <- of(2)
+
+	mut obs := from_channel(ch)
+	mut all := obs.all(fn (value ItemValue) bool {
+		return value is int
+	})
+
+	assert_single(mut ctx, mut all, has_item(false))
 }
 
 fn test_average_f32() {
@@ -46,7 +63,7 @@ fn test_average_f32() {
 	mut obs := from_channel(ch)
 	mut avg := obs.average_f32()
 
-	assert_single(mut ctx, mut avg, has_items(f32(1.0)))
+	assert_single(mut ctx, mut avg, has_item(f32(1.0)))
 }
 
 fn test_average_f64() {
@@ -66,7 +83,7 @@ fn test_average_f64() {
 	mut obs := from_channel(ch)
 	mut avg := obs.average_f64()
 
-	assert_single(mut ctx, mut avg, has_items(1.0))
+	assert_single(mut ctx, mut avg, has_item(1.0))
 }
 
 fn test_average_int() {
@@ -86,7 +103,7 @@ fn test_average_int() {
 	mut obs := from_channel(ch)
 	mut avg := obs.average_int()
 
-	assert_single(mut ctx, mut avg, has_items(1))
+	assert_single(mut ctx, mut avg, has_item(1))
 }
 
 fn test_average_i16() {
@@ -106,7 +123,7 @@ fn test_average_i16() {
 	mut obs := from_channel(ch)
 	mut avg := obs.average_i16()
 
-	assert_single(mut ctx, mut avg, has_items(i16(1)))
+	assert_single(mut ctx, mut avg, has_item(i16(1)))
 }
 
 fn test_average_i64() {
@@ -126,5 +143,5 @@ fn test_average_i64() {
 	mut obs := from_channel(ch)
 	mut avg := obs.average_i64()
 
-	assert_single(mut ctx, mut avg, has_items(i64(1)))
+	assert_single(mut ctx, mut avg, has_item(i64(1)))
 }
